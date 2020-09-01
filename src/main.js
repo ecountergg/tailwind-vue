1d6f1105f2d827c013ed0730348017a37d1dc138
ecountergg <ecountergg@gmail.com>
2020-09-01 20:27:49 +0700

    setup vee-validate

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import VueSweetalert2 from 'vue-sweetalert2';
+import { extend } from 'vee-validate'
+import * as rules from 'vee-validate/dist/rules'
+import { ValidationProvider, ValidationObserver } from 'vee-validate'
 import axios from 'axios'
 
 import routes from '@/routes'
@@ -14,7 +17,14 @@ if (token) {
   axios.defaults.headers.common['Authorization'] = `Bearer ${token}`
 }
 
+Object.keys(rules).forEach(rule => {
+    extend(rule, rules[rule])
+})
+
 Vue.use(VueSweetalert2);
+Vue.component('ValidationProvider', ValidationProvider)
+Vue.component('ValidationObserver', ValidationObserver)
+
 
 Vue.config.productionTip = false
 
