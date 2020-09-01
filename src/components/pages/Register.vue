4dd661a100f716c7b3e44ae61603feea171b81c3
ecountergg <ecountergg@gmail.com>
2020-09-01 21:00:30 +0700

    change method name

--- src/components/pages/Register.vue
+++ src/components/pages/Register.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="login" class="flex items-center">
         <div class="w-full max-w-xs mx-auto">
-            <ValidationObserver ref="createUser" v-slot="{ handleSubmit }">
+            <ValidationObserver ref="createUser" v-slot="{ handleRegister }">
                 <form class="bg-white shadow-lg rounded px-8 pt-6 pb-8 mb-4">
                     <div class="mb-4">
                         <ValidationProvider
@@ -76,7 +76,7 @@
                                 focus:outline-none focus:shadow-outline
                             "
                             type="button"
-                            @click="handleSubmit(register)"
+                            @click="handleRegister(register)"
                         >
                             Register
                         </button>
